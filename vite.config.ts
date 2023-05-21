981506caedf333d21eb41fe63ec2cb9e62edc0a2
thuanpm <thuanpm@sunshinegroup.vn>
2023-05-21 23:40:57 +0700

    udpate

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({insertTypesEntry:true})],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -18,7 +19,7 @@ export default defineConfig({
         globals: {
           react: 'React'
         }
-      }
+      },
     },
     emptyOutDir:true,
     outDir:"./dist",
