fdb48f0800a1324ad448f2d35d1d523bb199df9c
thuanpm <thuanpm@sunshinegroup.vn>
2023-05-21 20:10:45 +0700

    init rad-ts

---
+++ src/components/Button.tsx
@@ -0,0 +1,4 @@
+import "./button.css";
+export const Button=()=>{
+    return <span className="button">Ahihi</span>
+}
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+
+export {Button} from "./components/Button.tsx";
