03bc95e303f4db4f878d76a55683e9876d00db56
thuanpm <thuanpm@sunshinegroup.vn>
2023-05-21 21:40:15 +0700

    update test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'rad-ts',
+      formats: ['cjs', 'es'],
+      name: 'rad-ts',
+    },
+    emptyOutDir:true,
+    outDir:"./dist",
+    minify:true,
+    cssMinify:true,
+    sourcemap:true,
+  }
 })
